2607d855d0359e15a4de8f1661ae90e3de76e101
SerhiiMolodets <mr.molodets@gmail.com>
2023-08-22 18:37:30 +0300

    added project;

---
+++ Russia Looses/Russia Looses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Russia Looses
+//
+//  Created by Serhii Molodets on 22.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
